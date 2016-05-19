2cde17dc5b73196fa69d9d861937413159264b84
Andrew Brobston <andrew@brobston.com>
2016-05-19 10:56:51 -0500

    Ignore UMD patterns and two more support files in coverage
    Added two more, previously-overlooked files to the exclusion
    list for code coverage.  Added code-coverage ignore comments
    for each universal-module-definition statement.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 (function(root, factory) {
+    /* istanbul ignore next */
     if (typeof define === 'function' && define.amd) {
         define(["require", "./lib/jscc/main"], factory);
     } else if (typeof module === 'object' && module.exports) {
